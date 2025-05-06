b43d7db025f4a7bd8bbaa4b68fc56231b63ee128
Dave Leo Shilander <dave.leo.shilander@icloud.com>
2025-05-06 12:56:07 -0500

    update validation to use env variables

--- netlify/functions/validate.js
+++ netlify/functions/validate.js
@@ -1,54 +1,74 @@
 
+//
+// const answers = {
+// 	task1: {
+// 		answer: "a",
+// 		magicNumber: 1
+// 	},
+//
+// 	task2: {
+// 		answer: process.env[`TASK${task}_ANSWER`],
+// 		magicNumber: process.env[`TASK${task}_MAGIC`]
+// 	},
+//
+// 	task3: {
+// 		answer: "c",
+// 		magicNumber: 3
+// 	},
+//
+// 	task4: {
+// 		answer: "d",
+// 		magicNumber: 4
+// 	},
+//
+// 	task5: {
+// 		answer: "e",
+// 		magicNumber: 5
+// 	},
+//
+// 	task6: {
+// 		answer: "f",
+// 		magicNumber: 6
+// 	},
+// }
+//
 
-const answers = {
-	task1: {
-		answer: "a",
-		magicNumber: 1
-	},
-
-	task2: {
-		answer: "blue",
-		magicNumber: 2
-	},
-
-	task3: {
-		answer: "c",
-		magicNumber: 3
-	},
-
-	task4: {
-		answer: "d",
-		magicNumber: 4
-	},
-
-	task5: {
-		answer: "e",
-		magicNumber: 5
-	},
-
-	task6: {
-		answer: "f",
-		magicNumber: 6
-	},
-}
+// const validate = (event) => {
+// 	const {task, input} = event.queryStringParameters;
+//
+// 	if(answers[`task${task}`]?.answer === input){
+// 		return {
+// 			success: true,
+// 			magicNumber: answers[`task${task}`].magicNumber
+// 		};
+// 	}
+// 	else{
+// 		return {
+// 			success: false,
+// 			message: "Invalid input"
+// 		};
+// 	}
+// }
 
 
 const validate = (event) => {
-	const {task, input} = event.queryStringParameters;
+	const { task, input } = event.queryStringParameters;
 
-	if(answers[`task${task}`]?.answer === input){
+	const expectedAnswer = process.env[`TASK${task}_ANSWER`];
+	const magicNumber = process.env[`TASK${task}_MAGIC`];
+
+	if (expectedAnswer && input === expectedAnswer) {
 		return {
 			success: true,
-			magicNumber: answers[`task${task}`].magicNumber
+			magicNumber: Number(magicNumber),
 		};
-	}
-	else{
+	} else {
 		return {
 			success: false,
-			message: "Invalid input"
+			message: "Invalid input",
 		};
 	}
-}
+};
 
 exports.handler = async (event) => {
 	const result = validate(event);
